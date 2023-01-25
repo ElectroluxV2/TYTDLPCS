266d06e05082bec7e8976a930a2b53f2c638aa19
ElectroluxV2 <electroluxv2@gmail.com>
2023-01-25 22:17:37 +0100

    increase timeouts

--- TyranoKurwusBot/Core/Downloaders/DownloadManager.cs
+++ TyranoKurwusBot/Core/Downloaders/DownloadManager.cs
@@ -4,8 +4,8 @@ namespace TyranoKurwusBot.Core.Downloaders;
 
 public static partial class DownloadManager
 {
-    private static readonly TimeSpan ChildMaxTickTime = TimeSpan.FromSeconds(3);
-    private static readonly TimeSpan InstallCommandTimeout = TimeSpan.FromMinutes(1);
+    private static readonly TimeSpan ChildMaxTickTime = TimeSpan.FromSeconds(30);
+    private static readonly TimeSpan InstallCommandTimeout = TimeSpan.FromMinutes(5);
     private static readonly ILogger Logger = LoggerManager.Factory.CreateLogger(typeof(DownloadManager));
     public static readonly IDownloader YtDlp = new YtDlp();
 
