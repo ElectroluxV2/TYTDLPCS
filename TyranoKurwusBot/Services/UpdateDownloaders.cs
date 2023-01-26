4891ff22cc369796b05980517594a7ec1489e3d4
ElectroluxV2 <electroluxv2@gmail.com>
2023-01-26 17:19:59 +0100

    update downloaders on start

--- TyranoKurwusBot/Services/UpdateDownloaders.cs
+++ TyranoKurwusBot/Services/UpdateDownloaders.cs
@@ -4,18 +4,30 @@ namespace TyranoKurwusBot.Services;
 
 public class UpdateDownloaders : BackgroundService
 {
+    private readonly ILogger<UpdateDownloaders> _logger;
     private readonly IServiceScopeFactory _serviceScopeFactory;
-    public UpdateDownloaders(IServiceScopeFactory serviceScopeFactory) => _serviceScopeFactory = serviceScopeFactory;
+    public UpdateDownloaders(IServiceScopeFactory serviceScopeFactory, ILogger<UpdateDownloaders> logger)
+    {
+        _serviceScopeFactory = serviceScopeFactory;
+        _logger = logger;
+    }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        _logger.LogInformation("Updating all updaters now");
+        await ExecuteUpdate();
+        _logger.LogInformation("Complete, next update in 1 day");
+        
         var timer = new PeriodicTimer(TimeSpan.FromDays(1));
         while (await timer.WaitForNextTickAsync(stoppingToken))
         {
-            foreach (var downloader in DownloadManager.AvailableDownloaders)
-            {
-                await downloader.InstallOrUpdateAsync();
-            }
+            await ExecuteUpdate();
         }
+    }
+
+    private static async Task ExecuteUpdate()
+    {
+        var tasks = DownloadManager.AvailableDownloaders.Select(downloader => downloader.InstallOrUpdateAsync());
+        await Task.WhenAll(tasks);
     }
 }
